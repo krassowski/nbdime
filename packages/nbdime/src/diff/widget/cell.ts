a18296402f93427b192bcccd9247e057d71978d6
krassowski <5832902+krassowski@users.noreply.github.com>
2023-08-13 15:31:59 +0100

    Add "Cell ID" prefix

--- packages/nbdime/src/diff/widget/cell.ts
+++ packages/nbdime/src/diff/widget/cell.ts
@@ -129,7 +129,7 @@ class CellDiffWidget extends Panel {
           header.addWidget(w);
         }
         if (model.cellId && id !== null) {
-          let w = createWidget(id);
+          let w = createWidget(`Cell ID: ${id}`);
           w.addClass(CELL_ID_CLASS);
           FlexPanel.setGrow(w, 1);
           header.addWidget(w);
